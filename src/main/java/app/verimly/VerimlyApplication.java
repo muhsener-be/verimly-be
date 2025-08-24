5b21179c8a229f9fca4f33a52e6127446834904d
muhsener01 <muh.sener98@icloud.com>
2025-08-25 00:32:27 +0300

    first commit after create project

---
+++ src/main/java/app/verimly/VerimlyApplication.java
@@ -0,0 +1,13 @@
+package app.verimly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerimlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VerimlyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/verimly/VerimlyApplicationTests.java
@@ -0,0 +1,13 @@
+package app.verimly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerimlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
