5b7b3cc3e6ff69668c3fd1216c5d861194eebdfd
muhsener01 <muh.sener98@icloud.com>
2025-08-25 00:38:39 +0300

    prepared for multi-module development environment     - src folder deleted in parent base module     - app sub-module created to generate runnable jar file

--- app/src/main/java/app/verimly/VerimlyApplication.java
+++ app/src/main/java/app/verimly/VerimlyApplication.java
@@ -1,13 +1,12 @@
 package app.verimly;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VerimlyApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(VerimlyApplication.class, args);
     }
-
 }
